ece846e0f6f3f115ceeeed78bb1843ee63da0dcb
Kevin <stradtkt22@gmail.com>
2019-06-07 06:14:18 -0400

    created the navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">Staff Portal</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/staff">Staff</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/"></Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;

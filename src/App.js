dd3c4a44340fd6402cf7c30cf62055ebfdd66516
Kevin <stradtkt22@gmail.com>
2019-06-07 06:22:17 -0400

    created the home and staff page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <div className="container">
+                        <Route path="/" exact={true} component={Home}/>
+                        <Route path="/staff" exact={true} component={Staff}/>
+                    </div>
+                </div>
+            </Router>
         )
     }
 }

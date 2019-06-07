0ff41f3a821860e000be1bfade1102c7b988d5ff
Kevin <stradtkt22@gmail.com>
2019-06-07 08:56:58 -0400

    Added the Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {withAuth} from '@okta/okta-react';
 
-export default () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authenticated: null
+        };
+        this.checkAuthentication = this.checkAuthentication.bind(this);
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+    async checkAuthentication() {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if(authenticated !== this.state.authenticated) {
+            this.setState({authenticated});
+        }
+    }
+    async componentDidMount() {
+        this.checkAuthentication();
+    }
+    async componentDidUpdate() {
+        this.checkAuthentication();
+    }
+    async login() {
+        this.props.auth.login('/');
+    }
+    async logout() {
+        this.props.auth.logout('/');
+    }
+    render() {
+        if(this.state.authenticated === null) return null;
+        const button = this.state.authenticated ?
+            <button onClick={this.logout}>Logout</button> :
+            <button onClick={this.login}>Login</button>
+        return (
+            <div>
+                <Link to="/">Home</Link> <br/>
+                <Link to="/protected">Protected</Link> <br/>
+                {button}
+            </div>
+        );
+    }
 }
+
+export default withAuth(Home);
